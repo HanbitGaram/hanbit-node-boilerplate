e6f35236b9b4b85d84542dcaf976c8a101b4f006
한빛가람(ハンビッガラム) <webmaster@hanb.jp>
2024-01-21 20:55:21 +0900

    테스트

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({extended: true})
+);
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+app.use(express.static(__dirname + '/src/views'));
+
+// 중간에 들어갈 라우팅 규칙이나 기타 코드들이 여기에 삽입 될 예정
+
+app.get('/', (req, res)=>{
+    res.render('sample', {
+        title: '테스트'
+    });
+});
+
+app.listen(8080, '127.0.0.1', () => {
+    // 콘솔에 보일 로그
+    console.log(`서버가 서버주소:포트 에서 실행되었습니다.`);
+});
